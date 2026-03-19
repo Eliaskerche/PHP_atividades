dd13a3272e7e0af95fc29ce290f6fab75e4cf5ed
Eliaskerche <kercheelias464@gmail.com>
2026-03-19 17:27:18 -0300

    Add files via upload

---
+++ Ex8PHP/Ex8PHP/Ex8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio08PhP</title>
+</head>
+<body>
+    <form method="post" action="main.php">
+        <h1>PhP_Exercicio 8: </h1> <h2>Faça um programa que receba um numero inteiro, determinando a soma de 0 até ta numero (incluso) por meio de uma função soma(n) que retorne tal soma</h2>
+        <label>Escreva: </label>
+        <input type="text" name="n1" step="any" required>
+        <button>Enviar</button>
+    </form>
+</body>
+</html>
